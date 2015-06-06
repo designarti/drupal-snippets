a1a8f7ab6d49d7580c3fad0830a7292a30ddff07
designarti <designarti@yahoo.com>
2015-06-06 17:57:32 +0300

    Update and rename body-class-if-region-present to snippets.php

---
+++ snippets.php
@@ -0,0 +1,20 @@
+<?php
+//
+// Add CSS class to the body element based on published regions
+// In template.php
+function THEMENAME_preprocess_page(&$vars, $hook) {
+  $body_classes = array($vars['body_classes']);
+  if ($vars['region_machine_name']) {
+    $body_classes[] = 'desired-class';
+  }
+  $vars['body_classes'] = implode(' ', $body_classes);
+}
+//
+// Create a new function available in page.tpl.php
+// In template.php
+function THEMENAME_preprocess_page(&$variables) {
+  $variables['variable_name'] = t('The variable string or text');
+}
+// In page.tpl.php
+<?php print $variable_name; ?>
+?>
